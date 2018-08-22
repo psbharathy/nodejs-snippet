fe75e60c85fd81a4d4233bea67502186cd03184e
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-22 12:25:32 +0530

    Creating a new module

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 // Regulrar Javascript
+// Loding looger module
+var logger = require("./logger");
 
-function sayHello(name) {
-  console.log("Hello " + name);
-}
-
-sayHello("Bharathy");
-
-// Window is not defined
-// console.log(window);
-// console is glbal object
-// other global objects are => global.console.log
-// global.setInterval()
-
-// in Node every file is a module
-// inside file are scoped to that file
-console.log(module);
+logger.log("I am Logger");
---
+++ logger.js
@@ -0,0 +1,10 @@
+// Logging for various part of this projects
+
+var url = "http://mylooger.io/log";
+
+function log(message) {
+  //
+  console.log(message);
+}
+
+module.exports.log = log;
