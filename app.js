59b956eb00c240af56ad144fe266378c0cac6b78
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-22 12:52:24 +0530

    PATH, OS and FS in node

--- app.js
+++ app.js
@@ -2,5 +2,29 @@
 // Loding looger module
 var log = require("./logger");
 const path = require("path");
-log(path.parse(__filename));
-log("I am Logger");
+const os = require("os");
+
+const fs = require("fs");
+// Syncronus method
+// const files = fs.readdirSync("./");
+// log(`files :${files}`);
+
+// Asyncrhouns
+fs.readdir("./", function(err, files) {
+  if (err) console.log("error", err);
+  else log(`Result :${files}`);
+});
+
+/// Throw execeptions
+// fs.readdir("$", function(err, files) {
+//   if (err) console.log("error", err);
+//   else log(`Result :${files}`);
+// });
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+log(`Total Memeory :${totalMemory}`);
+log(`Free Memeory :${freeMemory}`);
+
+// log(path.parse(__filename));
+// log("I am Logger");
