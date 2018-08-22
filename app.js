72367e1b8c9cb2c2149f668ad9271b96d447be74
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-22 15:22:20 +0530

    Http Connection with server basics

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const path = require("path");
 const os = require("os");
 
 const fs = require("fs");
+
+// HTTP class
+const http = require("http");
+const server = http.createServer();
+server.on("connection", socket => {
+  console.log("New Connection");
+});
+server.listen(3000);
+console.log("Listening on Port 3000...");
+
 // Syncronus method
 // const files = fs.readdirSync("./");
 // log(`files :${files}`);
@@ -12,7 +22,7 @@ const fs = require("fs");
 // Asyncrhouns
 fs.readdir("./", function(err, files) {
   if (err) console.log("error", err);
-  else log(`Result :${files}`);
+  else console.log(`Result :${files}`);
 });
 
 /// Throw execeptions
@@ -23,8 +33,8 @@ fs.readdir("./", function(err, files) {
 
 var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
-log(`Total Memeory :${totalMemory}`);
-log(`Free Memeory :${freeMemory}`);
+console.log(`Total Memeory :${totalMemory}`);
+console.log(`Free Memeory :${freeMemory}`);
 
 // log(path.parse(__filename));
 // log("I am Logger");
