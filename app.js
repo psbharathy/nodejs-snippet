a3fd7fb13aa9575695e2af56ab7c42e409ad4efb
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-22 15:27:29 +0530

    Server Listen nd hell world

--- app.js
+++ app.js
@@ -8,33 +8,14 @@ const fs = require("fs");
 
 // HTTP class
 const http = require("http");
-const server = http.createServer();
-server.on("connection", socket => {
-  console.log("New Connection");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.write("Hello World");
+    res.end();
+    console.log("Port Listned");
+  }
 });
 server.listen(3000);
-console.log("Listening on Port 3000...");
 
-// Syncronus method
-// const files = fs.readdirSync("./");
-// log(`files :${files}`);
-
-// Asyncrhouns
-fs.readdir("./", function(err, files) {
-  if (err) console.log("error", err);
-  else console.log(`Result :${files}`);
-});
-
-/// Throw execeptions
-// fs.readdir("$", function(err, files) {
-//   if (err) console.log("error", err);
-//   else log(`Result :${files}`);
-// });
-
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-console.log(`Total Memeory :${totalMemory}`);
-console.log(`Free Memeory :${freeMemory}`);
-
-// log(path.parse(__filename));
-// log("I am Logger");
+console.log("Listening the port 3000");
