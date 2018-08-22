fb4ad4c492eb528c2aa0dc42b17143818194fb8d
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-22 12:16:05 +0530

    Module def

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ function sayHello(name) {
 }
 
 sayHello("Bharathy");
+
+// Window is not defined
+// console.log(window);
+// console is glbal object
+// other global objects are => global.console.log
+// global.setInterval()
+
+// in Node every file is a module
+// inside file are scoped to that file
+console.log(module);
