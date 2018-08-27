54aa47ae90e08d2c9c34d793b2cce3fc63067625
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-27 18:15:15 +0530

    Building Node web server with express

---
+++ express-snippet/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World !");
+});
+
+app.get("/api/category", (req, res) => {
+  res.send(["Air", "Books", "Cartoon"]);
+});
+
+app.listen(3000, () => console.log("Listening on Port 3000..."));
