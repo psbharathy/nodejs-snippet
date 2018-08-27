19372a8d4ab3160dfcba54a7f52593b5aee6be32
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-27 18:51:57 +0530

    Request Arg with query param

--- express-snippet/index.js
+++ express-snippet/index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World !");
+  res.send("Hello World !!!");
 });
 
 app.get("/api/category", (req, res) => {
   res.send(["Air", "Books", "Cartoon"]);
 });
 
-app.listen(3000, () => console.log("Listening on Port 3000..."));
+app.get("/api/category/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/items/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port ${port}...`));
