076260b53286d2613945818951bd1c263346b679
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-27 14:58:58 +0530

    Semantaic version

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -4,6 +4,9 @@ var _ = require("underscore");
 // Core Module
 // file or folder (./underscore)
 // node_modules
-
+// SemanticVersion SemVer
+// "^5.2.9" => Major.Minor.Patch
+// 5.x => Major is
+// ~5. => interested in 1.8.x
 var result = _.contains([1, 2, 3], 3);
 console.log(result);
