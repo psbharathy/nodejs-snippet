647e5e629bbe5fceeceec8350af9d3f28e9972ae
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-27 14:38:13 +0530

    Using Package

---
+++ npm-demo/index.js
@@ -0,0 +1,9 @@
+var _ = require("underscore");
+// rquires looks for three level
+//
+// Core Module
+// file or folder (./underscore)
+// node_modules
+
+var result = _.contains([1, 2, 3], 3);
+console.log(result);
