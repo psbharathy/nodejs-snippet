0850663565c69faf074a47cc8536413ccfeee25a
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-28 12:13:58 +0530

    Buildin Middleware - Helmet Morgan and Static

--- express-snippet/index.js
+++ express-snippet/index.js
@@ -2,10 +2,15 @@ const logger = require("./middleware/logger");
 const Joi = require("joi");
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 // This is a middleware priec
+app.use(helmet());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(morgan("tiny"));
 app.use(logger);
 
 // Creating Custom Middleware
