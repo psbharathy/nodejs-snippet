9ab12e3bd3772fc021c310ba1092f28c59f3cbdb
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-28 11:36:55 +0530

    HTTP delete and code refactor

--- express-snippet/index.js
+++ express-snippet/index.js
@@ -16,29 +16,34 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/categories", (req, res) => {
-  res.send(["Air", "Books", "Cartoon"]);
+  res.send(categories);
 });
 
-app.put("api/categories/:id", (req, res) => {
+app.put("/api/categories/:id", (req, res) => {
   // Look up the course
   // if not retrun 404
+  const category = categories.find(c => c.id === parseInt(req.params.id));
+  if (!category)
+    return res.status(404).send(`the Category not found ${req.params.id}`);
+
   // validate the course
+  // Object Destrurting {error}
+  const { error } = validateCatrgory(req.body); // result.error
+  if (error) return res.status(400).send(error.details[0].message);
+
   // update the coure
+  category.name = req.body.name;
+  res.send(category);
+
   // return updated course
 });
 
 app.post("/api/categories", (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-  const result = Joi.validate(req.body, schema);
+  // validate the course
+  // Object Destrurting {error}
+  const { error } = validateCatrgory(req.body); // result.error
+  if (error) return res.status(400).send(error.details[0].message);
 
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
   const category = {
     id: categories.length + 1,
     name: req.body.name
@@ -49,9 +54,28 @@ app.post("/api/categories", (req, res) => {
 app.get("/api/categories/:id", (req, res) => {
   const category = categories.find(c => c.id === parseInt(req.params.id));
   if (!category)
-    res.status(404).send(`the Category not found ${req.params.id}`);
+    return res.status(404).send(`the Category not found ${req.params.id}`);
   res.send(category);
 });
+
+app.delete("/api/categories/:id", (req, res) => {
+  const category = categories.find(c => c.id === parseInt(req.params.id));
+  if (!category)
+    return res.status(404).send(`the Category not found ${req.params.id}`);
+
+  const index = categories.indexOf(category);
+  categories.splice(index, 1);
+  res.send(category);
+});
+
+function validateCatrgory(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on Port ${port}...`));
