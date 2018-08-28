70f58c71f689b56b7e7e53711f7c5631b8a02f4a
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-28 15:36:18 +0530

    Compare with Sync and Async

--- async-snippet/index.js
+++ async-snippet/index.js
@@ -1,13 +1,40 @@
 console.log("Before");
-const user = getUser(1);
-console.log(user);
+
+// Example for CallBack
+getUser(1, user => {
+  getRepository(user.username, repos => {
+    getCommits(repos, commits => {});
+  });
+});
+
 console.log("After");
 
-function getUser(id) {
+// Synchronous example
+console.log("Syn Before");
+// const user = getUser(1);
+// const repos = getRepository(user.username);
+// const commits = getCommits(repos);
+console.log("Async After");
+
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading User from DB");
-    return { id: id, user: "Bharathy" };
+    callback({ id: id, username: "psbharathy" });
   }, 2000);
 
   return 1;
+}
+
+function getRepository(username, callback) {
+  setTimeout(() => {
+    console.log("Reading User Repos from API");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
+
+function getCommits(repos, callback) {
+  setTimeout(() => {
+    console.log("Reading Repos Commits from API");
+    callback(["Cmt1", "Cmt2", "Cmt3"]);
+  }, 2000);
 }
