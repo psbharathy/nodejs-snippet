b78ecd96f3e37d3e4b7034cccc40ee322549a6f7
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-28 09:54:02 +0530

    Handling Get Request

--- express-snippet/index.js
+++ express-snippet/index.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
 
+const categories = [
+  { id: 1, name: "Book" },
+  { id: 2, name: "Art" },
+  { id: 3, name: "Computer" }
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World !!!");
 });
 
-app.get("/api/category", (req, res) => {
+app.get("/api/categories", (req, res) => {
   res.send(["Air", "Books", "Cartoon"]);
 });
 
-app.get("/api/category/:id", (req, res) => {
-  res.send(req.params.id);
-});
-
-app.get("/api/items/:year/:month", (req, res) => {
-  res.send(req.query);
+app.get("/api/categories/:id", (req, res) => {
+  const category = categories.find(c => c.id === parseInt(req.params.id));
+  if (!category)
+    res.status(404).send(`the Category not found ${req.params.id}`);
+  res.send(category);
 });
 
 const port = process.env.PORT || 3000;
