bfb69f38cb3d7867a9517a6e3c780e6cc9964248
psbharathy <subramania.bharathy@compassitesinc.com>
2018-08-28 10:21:30 +0530

    HTTP Post with Joi Validation

--- express-snippet/index.js
+++ express-snippet/index.js
@@ -1,5 +1,9 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
+
+// This is a middleware priec
+app.use(express.json());
 
 const categories = [
   { id: 1, name: "Book" },
@@ -15,6 +19,25 @@ app.get("/api/categories", (req, res) => {
   res.send(["Air", "Books", "Cartoon"]);
 });
 
+app.post("/api/categories", (req, res) => {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const category = {
+    id: categories.length + 1,
+    name: req.body.name
+  };
+  categories.push(category);
+  res.send(category);
+});
 app.get("/api/categories/:id", (req, res) => {
   const category = categories.find(c => c.id === parseInt(req.params.id));
   if (!category)
