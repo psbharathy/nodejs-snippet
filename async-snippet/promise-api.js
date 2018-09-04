a8ef9d42d310c8359ba1cb8f5220d7dd8ad92647
psbharathy <subramania.bharathy@compassitesinc.com>
2018-09-04 11:26:06 +0530

    Promis with race method

--- async-snippet/promise-api.js
+++ async-snippet/promise-api.js
@@ -1,7 +1,8 @@
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log("Async Operation 1..");
-    reject(new Error("Async Op 1 rejected"));
+    // reject(new Error("Async Op 1 rejected"));
+    resolve(1);
   }, 2000);
 });
 
@@ -9,9 +10,16 @@ const p2 = new Promise(resolve => {
   setTimeout(() => {
     console.log("Async Operation 2..");
     resolve(2);
-  }, 2000);
+  }, 1000);
 });
 
 Promise.all([p1, p2])
   .then(result => console.log(result))
   .catch(err => console.log("Erorr", err.message));
+// Output [1,2]
+
+// Promise.race([p1, p2])
+//   .then(result => console.log(result))
+//   .catch(err => console.log("Erorr", err.message));
+
+// Output  2
