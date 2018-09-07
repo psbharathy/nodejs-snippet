6e513c71b7bc201e64e61b303e4695eeb2dcbcd9
psbharathy <subramania.bharathy@compassitesinc.com>
2018-09-07 11:29:30 +0530

    Async validator with callback

--- mongo-snippet/index.js
+++ mongo-snippet/index.js
@@ -11,8 +11,13 @@ const courseSchema = new mongoose.Schema({
   tags: {
     type: String,
     validate: {
-      validator: function(v) {
-        return v && v.length > 0;
+      // Async Validator with callback
+      isAsync: true,
+      validator: function(v, callback) {
+        setTimeout(() => {
+          const result = v && v.length > 0;
+          callback(result);
+        }, 4000);
       },
       message: "A course should have at least one tag"
     }
