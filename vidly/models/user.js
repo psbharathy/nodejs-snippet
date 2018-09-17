18b38da3b88c3c777a35bd6de2d3a75908d7099a
psbharathy <subramania.bharathy@compassitesinc.com>
2018-09-17 14:49:15 +0530

    Users API update

--- vidly/models/user.js
+++ vidly/models/user.js
@@ -4,20 +4,41 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 255
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 5,
+    maxlength: 1024
   }
 });
 const User = mongoose.model("Users", userSchema);
 
 function validateUser(user) {
   const schema = {
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    password: Joi.string().required()
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
   };
   return Joi.validate(user, schema);
 }
--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -8,8 +8,23 @@ router.get("/me", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const error = await validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  try {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send("User Already Registered");
+    users = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+    users = await users.save();
+
+    res.send(users);
+  } catch (ex) {
+    res.send(ex);
+  }
 });
 
 module.exports = router;
