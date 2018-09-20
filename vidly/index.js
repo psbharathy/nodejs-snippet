da65c84429819ec19f2cf1cf81a12abc6d6573ad
psbharathy <subramania.bharathy@compassitesinc.com>
2018-09-20 18:48:33 +0530

    Refactor index and logging

--- vidly/index.js
+++ vidly/index.js
@@ -1,22 +1,12 @@
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
+const winston = require("winston");
 const express = require("express");
 const app = express();
 
 require("./startup/loging")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
-// Example Un Caught Exceptions Sync
-// throw new Error("Something Failed During StartUp..!");
-// Asynchrouns UnhandledPromise
-// const p = Promise.reject(new Error("Async Somtheing Failed"));
-// p.then(() => console.log("Done"));
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("jwtPrivateKey key is not defined");
-  process.exit(1);
-}
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
--- vidly/startup/loging.js
+++ vidly/startup/loging.js
@@ -3,7 +3,14 @@ const winston = require("winston");
 require("winston-mongodb");
 
 module.exports = function() {
+  // Example Un Caught Exceptions Sync
+  // throw new Error("Something Failed During StartUp..!");
+  // Asynchrouns UnhandledPromise
+  // const p = Promise.reject(new Error("Async Somtheing Failed"));
+  // p.then(() => console.log("Done"));
+
   winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.File({ filename: "logs/unCaughtErrors.log" })
   );
   // asynchrous code
