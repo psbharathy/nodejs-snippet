27a7928bff2a09729eb83c40b88b14e3a6e4e5af
psbharathy <subramania.bharathy@compassitesinc.com>
2018-09-21 12:52:23 +0530

    test cover with absolute and refactor

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,19 +1,20 @@
-//
 const lib = require("../lib");
-test("absolute - should return a +number if input is +", () => {
-  //
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
 
-test("absolute - should return a -negative if input is -", () => {
-  //
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
+describe("absolute", () => {
+  it(" should return a +number if input is +", () => {
+    //
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+  it("absolute - should return a -negative if input is -", () => {
+    //
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return 0 if input is negtaive", () => {
-  //
-  const result = lib.absolute(0);
-  expect(result).toBe(1);
+  it("absolute - should return 0 if input is negtaive", () => {
+    //
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
